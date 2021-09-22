2f00ddf275f426b9b4c96aa6bbaf3d36f90e18f3
Jyke321 <jyke321@gmail.com>
2021-09-22 09:59:04 -0400

    ex24 pseudocode outline

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,29 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jacob Cordonero
+ */
+
+public class Solution24 {
+    public static void main(String[] args) {
+        //prompt the user for both input strings
+        //then display the output as shown in the example that follows.
+        /*
+        Enter two strings and I'll tell you if they are anagrams:
+        Enter the first string: note
+        Enter the second string: tone
+        "note" and "tone" are anagrams.
+         */
+        //**replace note and tone with user input**
+    }
+
+    public boolean isAnagram(String word1, String word2) {
+        //determines if two words are anagrams of each other
+        //first arrange both words in alphabetical order
+        //then use string compare
+        //if they are equal they are anagrams so return true
+        //if they are not equal return false
+        //return true or false respectively
+    }
+}
