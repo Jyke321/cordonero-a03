b02969c63f1eea4ceac829059437077e47856292
Jyke321 <jyke321@gmail.com>
2021-09-29 17:30:21 -0400

    ex27 finished

--- exercise27/src/main/java/baseline/Solution27.java
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -1,40 +1,5 @@
 package baseline;
 
-/*
-Write a program that prompts for a first name, last name, employee ID, and ZIP code.
-Ensure that the input is valid according to these rules:
-
-The first name must be filled in.
-The last name must be filled in.
-The first and last names must be at least two characters long.
-An employee ID is in the format AA-1234. So, two letters, a hyphen, and four numbers.
-The ZIP code must be a number.
-Display appropriate error messages on incorrect data.
-
-Example Output
-Enter the first name: J
-Enter the last name:
-Enter the ZIP code: ABCDE
-Enter the employee ID: A12-1234
-The first name must be at least 2 characters long.
-The last name must be at least 2 characters long.
-The last name must be filled in.
-The employee ID must be in the format of AA-1234.
-The zipcode must be a 5 digit number.
-Or
-
-Enter the first name: John
-Enter the last name: Johnson
-Enter the ZIP code: 55555
-Enter the employee ID: TK-4321
-There were no errors found.
-Constraints
-Create a function for each validation rule.
-Then create a validateInput function that takes in all of the input data and
-invokes the specific validation functions.
-Use a single output statement to display the outputs.
- */
-
 import java.util.Scanner;
 
 public class Solution27 {
@@ -43,51 +8,105 @@ public class Solution27 {
     public static void main(String[] args) {
         //prompt the user for a first name, last name, employee id, and zipcode
         //get user input from a function for each data
+        String firstName = getUserInput("Enter the first name: ");
+        String lastName = getUserInput("Enter the last name: ");
+        String zipCode = getUserInput("Enter the ZIP code: ");
+        String employeeID = getUserInput("Enter the employee ID: ");
         //call the validateInput function giving it all the data
+        String buffer = validateInput(firstName,lastName,zipCode,employeeID);
         //print the String retrieved from validateInput
+        System.out.println(buffer);
     }
-    public static String validateInput(String firstName, String lastName, String employeeID,
-                                     String zipCode) {
+    private static String getUserInput(String prompt) {
+        System.out.print(prompt);
+        return in.nextLine();
+    }
+    public static String validateInput(String firstName, String lastName,
+                                       String zipCode, String employeeID) {
         //create a string to hold whether an error occurred
+        String buffer = "";
         //call the validateFirstName function and store whether there were any errors
+        buffer += validateFirstName(firstName);
         //call the validateLastName function and store whether there were any errors
-        //call the validateEmployeeID function and store whether there were any errors
+        buffer += validateLastName(lastName);
         //call the validateZipCode function and store whether there were any errors
+        buffer += validateZipCode(zipCode);
+        //call the validateEmployeeID function and store whether there were any errors
+        buffer += validateEmployeeId(employeeID);
         //if no errors occurred store "There were no errors found."
         //return the String
-        return "There were no errors found.";
+        if (buffer.compareTo("") == 0)
+            buffer = "There were no errors found.";
+        return buffer;
     }
     public static String validateFirstName (String name) {
         //create an empty string
-        //determine if first name is empty
-        //concatenate "The first name must be filled in." if there was an error
+        String buffer = "";
         //determine if first name is at least 2 characters
         //concatenate "The first name must be at least 2 characters long." if there was an error
+        if (name.length()<2)
+            buffer += "The first name must be at least 2 characters long.\n";
+        //determine if first name is empty
+        //concatenate "The first name must be filled in." if there was an error
+        if (name.compareTo("")==0) {
+            buffer += "The first name must be filled in.\n";
+        }
         //return the concatenated string
-        return "";
+        return buffer;
     }
     public static String validateLastName (String name) {
         //create an empty string
+        String buffer = "";
         //determine if last name is empty
         //concatenate "The last name must be filled in." if there was an error
+        if (name.length()<2)
+            buffer += "The last name must be at least 2 characters long.\n";
         //determine if last name is at least 2 characters
         //concatenate "The first name must be at least 2 characters long." if there was an error
+        if (name.compareTo("")==0) {
+            buffer += "The last name must be filled in.\n";
+        }
         //return the concatenated string
-        return "";
+        return buffer;
     }
     public static String validateEmployeeId (String id) {
         //create an empty string
+        String buffer = "";
         //determine if employee id is in format AA-1234
         //concatenate "The employee ID must be in the format of AA-1234." if there was an error
+        if (    (id.length() != 7) ||
+                !Character.isLetter(id.charAt(0)) ||
+                !Character.isLetter(id.charAt(1)) ||
+                id.charAt(2) != '-' ||
+                !Character.isDigit(id.charAt(3)) ||
+                !Character.isDigit(id.charAt(4)) ||
+                !Character.isDigit(id.charAt(5)) ||
+                !Character.isDigit(id.charAt(6)) )
+        {
+            buffer += "The employee ID must be in the format of AA-1234.\n";
+        }
         //return the concatenated string
-        return "";
+        return buffer;
     }
     public static String validateZipCode (String zipCode) {
         //create an empty string
+        String buffer = "";
         //determine if the ZIP code is only numbers
         //determine if the ZIP code is 5 numbers long (length = 5)
-        //concatenate "The zipcode must be a 5 digit number." if there was an error
+        //concatenate "The zipcode must be a 5-digit number." if there was an error
+        if(zipCode.length() != 5 || containsNonNumbers(zipCode))
+            buffer += "The zipcode must be a 5 digit number.\n";
         //return the concatenated string
-        return "";
+        return buffer;
+    }
+    public static boolean containsNonNumbers(String str) {
+        //create boolean to determine if string contains non-number
+        boolean nonNumber = false;
+        for (int i=0; i<str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i)))
+                nonNumber = true;
+        }
+        //return whether
+        return nonNumber;
     }
 }
--- exercise27/src/test/java/baseline/Solution27Test.java
+++ exercise27/src/test/java/baseline/Solution27Test.java
@@ -5,9 +5,43 @@ import org.junit.jupiter.api.Test;
 
 class Solution27Test {
     @Test
-    public void validateInputTest() {
+    public void validateInputTestForValidInput() {
         Assertions.assertEquals("There were no errors found.",
-                Solution27.validateInput("Jack","Daniels","AA-4386","32765"));
+                Solution27.validateInput("Jack","Daniels","32765","AA-4386"));
+        }
+    @Test
+    public void validateInputTestForInvalidEmployeeID() {
+        Assertions.assertEquals("The employee ID must be in the format of AA-1234.\n",
+                Solution27.validateInput("Jack","Daniels","32765","AAA4386"));
     }
-
+    @Test
+    public void validateInputTestForInvalidFirstName() {
+        Assertions.assertEquals("The first name must be at least 2 characters long.\n" +
+                        "The first name must be filled in.\n",
+                Solution27.validateInput("","Daniels","32765","AA-4386"));
+    }
+    @Test
+    public void validateZipCodeTestInvalid() {
+        Assertions.assertEquals("The zipcode must be a 5 digit number.\n",Solution27.validateZipCode("327658"));
+    }
+    @Test
+    public void validateZipCodeTestValid() {
+        Assertions.assertEquals("",Solution27.validateZipCode("32765"));
+    }
+    @Test
+    public void containsNonNumbersTestTrue() {
+        Assertions.assertEquals(true,Solution27.containsNonNumbers("12h45"));
+    }
+    @Test
+    public void containsNonNumbersTestFalse() {
+        Assertions.assertEquals(false,Solution27.containsNonNumbers("12345"));
+    }
+    @Test
+    public void validateEmployeeIDTestInvalid() {
+        Assertions.assertEquals("The employee ID must be in the format of AA-1234.\n",Solution27.validateEmployeeId("327658"));
+    }
+    @Test
+    public void validateEmployeeIDTestValid() {
+        Assertions.assertEquals("",Solution27.validateEmployeeId("AA-4386"));
+    }
 }
