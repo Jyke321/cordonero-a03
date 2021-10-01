c3ab63bc59bf33ed89e10a54044138a5972f3838
Jyke321 <jyke321@gmail.com>
2021-09-30 21:24:21 -0400

    ex30 finished

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -27,26 +27,35 @@ public class Solution30 {
 
         //create function that makes  a 2 dimensional array
         // with row and column length and width of 12 each.
-
+        int[][] multiplicationTable = solution.create12by12MultiplicationTableMatrix();
         //create a function that displays the matrix with
         // the desired alignment
-
+        solution.display12by12MultiplicationTableMatrix(multiplicationTable);
     }
     public int[][] create12by12MultiplicationTableMatrix() {
         int[][] multiplicationTable = new int[12][12];
         //uses a loop for the row
-
+        for (int i=0; i<12; i++) {
             //uses a loop for the column
-
+            for (int j=0; j<12; j++) {
                 //sets multiplicationTable[row][column] to the product of row and column
-
+                multiplicationTable[i][j] = (i+1)*(j+1);
+                //i+1 and j+1 since they start at 0 for the index but start at 1 when multiplying
+            }
+        }
         return multiplicationTable;
     }
-    public void display12by12MultiplicationTableMatrix() {
+    public void display12by12MultiplicationTableMatrix(int[][] multiplicationTable) {
         //use a nested loop to get each value
-            //print the value using indents without using the tab character
-            //(I have no clue how to do that, but I know c had formatting stuff, so maybe I should look at documentation here)
-            //nah, stack overflow sounds easier
+        for (int i=0;i<12;i++) {
+            for (int j=0; j<12;j++) {
+                //print the value using indents without using the tab character
+                System.out.printf("%5d", multiplicationTable[i][j]);
+                //I tried the java docs but ended up using geeksforgeeks.org
+                // when I realized I was looking for a format option
+            }
+            System.out.println();
+        }
         //return nothing
     }
 }
