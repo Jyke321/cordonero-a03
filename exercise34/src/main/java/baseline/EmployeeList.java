48d5d9a7053b9ce3d85a375f3431618bf9877222
Jyke321 <jyke321@gmail.com>
2021-10-02 20:10:45 -0400

    ex34 pseudo code

---
+++ exercise34/src/main/java/baseline/EmployeeList.java
@@ -0,0 +1,23 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class EmployeeList {
+    ArrayList<String> employees = new ArrayList<String>(Arrays.asList("John Smith", "Jackie Jackson", "Chris Jones",
+            "Amanda Cullen", "Jeremy Goodwin"));
+    public void displayEmpolyees() {
+        //prints each employee in order on separate lines
+    }
+    public void removeEmployees() {
+        //removes specific employee
+    }
+    private int findEmployeeIndex() {
+        //looks through array list to find employee
+        return 0;
+    }
+    public int getNumberOfEmployees() {
+        //gets number of employees
+        return 0;
+    }
+}
---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,44 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jacob Cordonero
+ */
+
+import java.util.Scanner;
+
+/*
+Sometimes you have to locate and remove an entry from a list based on some criteria. You may have a deck of cards and need to discard one so it’s no longer in play, or you may need to remove values from a list of valid inputs once they’ve been used. Storing the values in an array makes this process easier. Depending on your language, you may find it safer and more efficient to create a new array instead of modifying the existing one.
+
+Create a small program that contains a list of employee names. Print out the list of names when the program runs the first time. Prompt for an employee name and remove that specific name from the list of names. Display the remaining employees, each on its own line.
+
+Example Output
+There are 5 employees:
+John Smith
+Jackie Jackson
+Chris Jones
+Amanda Cullen
+Jeremy Goodwin
+
+Enter an employee name to remove: Chris Jones
+
+There are 4 employees:
+John Smith
+Jackie Jackson
+Amanda Cullen
+Jeremy Goodwin
+Constraint
+Use an array or list to store the names.
+ */
+public class Solution34 {
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //display the employees to start with
+
+        //prompt user for an employee to remove
+
+        //display remaining employees
+
+    }
+}
