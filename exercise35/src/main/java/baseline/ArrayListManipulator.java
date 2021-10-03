1648a6333836ca39378ed8ecb61cac5e54a30221
Jyke321 <jyke321@gmail.com>
2021-10-02 21:16:11 -0400

    ex25 pseudocode

---
+++ exercise35/src/main/java/baseline/ArrayListManipulator.java
@@ -0,0 +1,24 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jacob Cordonero
+ */
+
+import java.util.ArrayList;
+
+public class ArrayListManipulator {
+    private ArrayList<String> array = new ArrayList<>();
+
+    public void addToArrayList(String item) {
+        //adds the item to the array list
+        //if "" don't add it
+
+    }
+    public String pickWinnerFromArrayList() {
+        //uses the rand int function to pick an index
+
+        //then returns the string associated with the index
+        return "";
+    }
+}
---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,46 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jacob Cordonero
+ */
+
+/*
+Arrays don’t have to be hard-coded.
+You can take user input and store it in an array and then work with it.
+
+Create a program that picks a winner for a contest or prize drawing.
+Prompt for names of contestants until the user leaves the entry blank.
+Then randomly select a winner.
+
+Example Output
+Enter a name: Homer
+Enter a name: Bart
+Enter a name: Maggie
+Enter a name: Lisa
+Enter a name: Moe
+Enter a name:
+The winner is... Maggie.
+Constraints
+Use a loop to capture user input into an array.
+Use a random number generator to pluck a value from the array.
+Don’t include a blank entry in the array.
+Some languages require that you define the length of the array ahead of time. You may need to find another data structure, like an ArrayList.
+ */
+
+import java.util.Scanner;
+
+public class Solution35 {
+    private static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //create ArrayListManipulator object
+
+        //start a loop that ends when user inputs ""
+            //prompt the user for a name
+
+            //add it to the array list
+
+        //pick a winner with object
+    }
+}
