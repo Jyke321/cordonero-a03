aef87c092f301fa3cb880bd8e0eb02982b5e9c33
Jyke321 <jyke321@gmail.com>
2021-10-03 22:08:38 -0400

    ed 39 pseudo, I'm cutting it close

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,42 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jacob Cordonero
+ */
+
+import java.awt.*;
+
+public class Solution39 {
+    //what I'm understanding is use a lit to store hash maps that each have all the data for a single person
+    public static void main(String[] args) {
+        //call function to store the data in a list of hash maps
+
+        //call a function to sort the list
+
+        //display the list of hashmaps in the fancy chart format
+
+    }
+    public List storeDataFromDataSet() {
+        //make list
+
+        //make first hashmap of data and add it to list
+
+        //make next hashmap for next person and so on
+
+        //return the list
+        return new List();
+    }
+    public List sortListByLastName() {
+        //make some sort of sorting algorithm
+        //maybe make a list of just the last names and sort those
+        //and then move the hash maps accordingly, not sure really
+        return new List();
+    }
+    public void displayListOfHashMapsInTabularFormat() {
+        //display top of table
+
+        //then use a loop to display rest of list
+        
+    }
+}
